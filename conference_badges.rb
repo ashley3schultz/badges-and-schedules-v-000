4ec7d6323ae7aa470fbe6de16ec2eb330f360ee6
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-19 01:33:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,8 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each {|badge|
-  badge}
+  puts "#{badge}"}
   batch_badge_creator(attendees).each {|room|}
-  room
+  puts "#{room}"}
 end
   
